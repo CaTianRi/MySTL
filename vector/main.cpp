6a3d4b40d2d1f0e80656fe05e38ed63eeb94f7b6
CaTianRi <1939653182@qq.com>
2023-10-30 15:24:16 +0000

    update vector

---
+++ vector/main.cpp
@@ -0,0 +1,24 @@
+#include "vector.h"
+#include <iostream>
+
+using namespace std;
+
+void test_1()
+{
+    bit::vector<int> nums;
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(4);
+    nums.push_back(5);
+    nums.push_back(6);
+    for(auto& it : nums)
+        cout << it << ' ';
+    cout << endl;
+}
+
+int main()
+{
+
+    return 0;
+}
