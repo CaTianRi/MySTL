a4510e07e1e32d61f76ec63b1bbd2eaf423bba7b
CaTianRi <1939653182@qq.com>
2023-10-30 12:08:30 +0800

    update vector

--- vector/vector.h
+++ vector/vector.h
@@ -2,4 +2,17 @@
 
 #include <iostream>
 
-
+namespace bit 
+{
+    template<class T>
+    class vector 
+    {
+        private:
+            T* _data;
+            size_t _capacity;
+            size_t _size;
+        public:
+            vector<T>();
+            vector<T>(size_t size, size_t val);
+    };
+};
