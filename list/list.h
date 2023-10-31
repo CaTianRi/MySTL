da3ac3edc21b8752098643aac69a14726e1fe7a9
CaTianRi <1939653182@qq.com>
2023-10-31 14:12:43 +0000

    update list

--- list/list.h
+++ list/list.h
@@ -23,20 +23,58 @@ namespace bit
     template <class T, class Ref, class Ptr>
     class ListIterator
     {
-        typedef list_node<T>* Node;
+        typedef list_node<T> Node;
         typedef ListIterator<T, Ref, Ptr> self;
     public:
-        ListIterator();
-        T& operator*();
-        T* operator->();
-        self& operator++();
-        self operator++(int);
-        self& operator--();
-        self& operator--(int);
-        bool operator!=(const self& l);
-        bool operator==(const self& l);
+        ListIterator(Node* node = nullptr)
+            :_node(node) 
+        {}
+
+        ListIterator(const self& l) :_node(l._node) {} 
+
+        T& operator*() { return _node->_data; };
+        T* operator->() { return _node->_next; };
+
+        self& operator++() 
+        {
+            _node = _node->_next;
+            return _node;
+        };
+
+        self operator++(int)
+        {
+            self tem(*this);
+            _node = _node->_next;
+
+            return tem;
+        }
+
+        self& operator--()
+        {
+            _node = _node->_perv;
+
+            return _node;
+        }
+
+        self& operator--(int)
+        {
+            self tem(_node);
+            _node = _node->prev;
+            return tem;
+        }
+
+        bool operator!=(const self& l)
+        {
+            return _node != l._node;
+        }
+
+        bool operator==(const self& l)
+        {
+            return _node == l._node;
+        }
+
     private:
-        Node _node;
+        Node* _node;
     };
 
     template <class T>
@@ -48,27 +86,27 @@ namespace bit
         typedef ListIterator<const T, const T&, const T*> const_iterator;
 
         // list();
-        list();
+        list() { init(); };
         list(int n, const T& value = T());
         list(const list<T>& l);
         list<T>& operator=(const list<T>& l); // or const list<T> l
         ~list();
         
         //list iterator
-        iterator begin();
-        iterator end();
-        const_iterator begin() const;
-        const_iterator end() const;
+        iterator begin() { return _node->_next; }
+        iterator end() { return _node; }
+        const_iterator begin() const { return _node->_next; }
+        const_iterator end() const { return _node; }
         
         //list capacity
-        size_t size() const;
-        bool empty() const;
+        size_t size() const { return _node->_perv - _node; }
+        bool empty() const { return _node->_next == _node; }
 
         //list access
-        T& front();
-        const T& front() const;
-        T& back();
-        const T& back() const;
+        T& front() { return _node->_next; }
+        const T& front() const { return _node->_next; } 
+        T& back() { return _node->_perv; }
+        const T& back() const { return _node->_perv; }
 
         //list Modify
         void push_back(const T& val);
@@ -80,6 +118,40 @@ namespace bit
         void swap(list<T>& l);
     private:
         void init();
-        Node* node;
+        Node* _node;
     };
+
+    template <class T>
+    void list<T>::init()
+    {
+        _node = new Node;
+        _node->_perv = _node;
+        _node->_next = _node;
+    }
+
+    template <class T>
+    list<T>::list(int n, const T& value)
+    {
+        for(int i = 0; i < n; ++i)
+        {
+            push_back(value);
+        }
+    }
+
+    template <class T>
+    void list<T>::push_back(const T& val)
+    {
+        Node* cur = new Node(val);
+        cur->_next = _node;
+        cur->_perv = _node->_perv;
+        _node->_perv = cur;
+    }
+
+    template <class T>
+    void list<T>::pop_back()
+    {
+        delete _node->_perv;
+    }
+
+
 }
