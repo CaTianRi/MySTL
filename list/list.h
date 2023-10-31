d37455c99a1cdc812155ed3223e8558c21874700
CaTianRi <1939653182@qq.com>
2023-10-31 13:01:28 +0000

    update list

--- list/list.h
+++ list/list.h
@@ -11,16 +11,32 @@ namespace bit
         list_node(const T& data = T())
             :_data(data)
             ,_next(nullptr)
-            ,_perv(nullptr)
-            {
-            }
+            ,_perv(nullptr) {}
+    };
+    
+    template <class Iterator, class Ref, class Ptr>
+    struct Reverse_iterator 
+    {
+        Iterator _it;
     };
 
-    template <class T>
-    struct _list_iterator
+    template <class T, class Ref, class Ptr>
+    class ListIterator
     {
-        list_node<T>* _node;
-        
+        typedef list_node<T>* Node;
+        typedef ListIterator<T, Ref, Ptr> self;
+    public:
+        ListIterator();
+        T& operator*();
+        T* operator->();
+        self& operator++();
+        self operator++(int);
+        self& operator--();
+        self& operator--(int);
+        bool operator!=(const self& l);
+        bool operator==(const self& l);
+    private:
+        Node _node;
     };
 
     template <class T>
@@ -28,8 +44,42 @@ namespace bit
     {
     public:
         typedef list_node<T> Node;
-        typedef _list_iterator<T> iterator;
+        typedef ListIterator<T, T&, T*> iterator;
+        typedef ListIterator<const T, const T&, const T*> const_iterator;
+
+        // list();
+        list();
+        list(int n, const T& value = T());
+        list(const list<T>& l);
+        list<T>& operator=(const list<T>& l); // or const list<T> l
+        ~list();
+        
+        //list iterator
+        iterator begin();
+        iterator end();
+        const_iterator begin() const;
+        const_iterator end() const;
+        
+        //list capacity
+        size_t size() const;
+        bool empty() const;
+
+        //list access
+        T& front();
+        const T& front() const;
+        T& back();
+        const T& back() const;
+
+        //list Modify
+        void push_back(const T& val);
+        void pop_back();
+        void pop_front();
+        iterator insert(iterator pos, const T& val);
+        iterator erase(iterator pos);
+        void clear();
+        void swap(list<T>& l);
     private:
+        void init();
         Node* node;
     };
 }
