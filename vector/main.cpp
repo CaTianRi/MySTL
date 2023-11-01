6c35b0ba8104fd736dbd85f3df2467e077ef4c7d
CaTianRi <1939653182@qq.com>
2023-11-01 01:30:51 +0000

    update vector

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,4 +1,4 @@
-#include "vector.h"
+#include "vector.hpp"
 
 using namespace std;
 
@@ -15,10 +15,46 @@ void test_1()
     for(auto it : nums)
         cout << it << " ";
     cout << endl;
+
+    bit::vector<int>::iterator it = nums.begin();
+    while(it != nums.end())
+    {
+        if(*it % 2 == 0)
+            *it += 10;
+        cout << *it << " ";
+        ++it;
+    }
+    cout << endl;
+
+    for(size_t i = 0; i < nums.size(); ++i)
+        cout << nums[i] << " ";
+    cout << endl << endl;
+}
+
+void test_2()
+{
+    int i = 0;
+    bit::vector<int*> v1;
+    v1.resize(10);
+    bit::vector<string> v2;
+    v2.resize(10, "xxx");
+
+    for(auto e : v1)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+
+    for(auto e : v2)
+    {
+        cout << e << " ";
+    }
+    cout << endl << endl;
 }
 
 int main()
 {
     test_1();
+    test_2();
     return 0;
 }
